3d40bfdc4912b9f8a3f7f151bb33dd1ced1be3e8
EunJaeJo <joeunjae0925@gmail.com>
2023-12-01 21:49:05 +0900

    modify Dice (to panel)

--- src/main/java/client/main/mainmap/Dice.java
+++ src/main/java/client/main/mainmap/Dice.java
@@ -7,23 +7,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Dice {
-    private JFrame frame;
+public class Dice extends JPanel {
     private JPanel cardPanel;
     private CardLayout cardLayout;
     private Timer rollTimer;
-    private int currentDiceResult; // 현재 주사위 결과를 저장할 변수
-    private JLabel[] diceLabels; // 주사위 이미지를 담을 배열
+    private JLabel[] diceLabels;
+    private int currentDiceResult;
 
     public Dice() {
-        frame = new JFrame("Dice Roller");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
         cardPanel = new JPanel();
         cardLayout = new CardLayout();
         cardPanel.setLayout(cardLayout);
 
-        // 주사위 이미지 경로 배열 (1부터 6까지)
         String[] imagePaths = {
                 "src/main/java/client/main/mainmap/img/dice1.png",
                 "src/main/java/client/main/mainmap/img/dice2.png",
@@ -33,21 +30,17 @@ public class Dice {
                 "src/main/java/client/main/mainmap/img/dice6.png"
         };
 
-        diceLabels = new JLabel[6]; // 주사위 이미지를 담을 배열 초기화
+        diceLabels = new JLabel[6];
 
         for (int i = 0; i < 6; i++) {
             ImageIcon imageIcon = new ImageIcon(imagePaths[i]);
             JLabel label = new JLabel(imageIcon);
-            diceLabels[i] = label; // 배열에 주사위 이미지 저장
+            diceLabels[i] = label;
             cardPanel.add(label, String.valueOf(i + 1));
         }
 
-        frame.add(cardPanel, BorderLayout.CENTER);
+        add(cardPanel, BorderLayout.CENTER);
 
-        JButton rollButton = new JButton("Roll Dice");
-        frame.add(rollButton, BorderLayout.SOUTH);
-
-        // 타이머 초기화
         rollTimer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -55,18 +48,17 @@ public class Dice {
             }
         });
 
-        // 스페이스바를 눌렀을 때 타이머 시작 또는 멈춤
-        frame.addKeyListener(new KeyListener() {
+        addKeyListener(new KeyListener() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_SPACE) {
                     if (rollTimer.isRunning()) {
                         rollTimer.stop();
-                        // 주사위 굴리기가 멈출 때 현재 주사위 결과를 가져옴
-                        currentDiceResult = getCurrentDiceResult();
+                        currentDiceResult = getCurrentVisibleDiceResult();
                         System.out.println("주사위 멈춤. 결과: " + currentDiceResult);
                     } else {
                         rollTimer.start();
+                        startDiceRollThread(); // 주사위 굴리는 스레드 시작
                     }
                 }
             }
@@ -80,44 +72,47 @@ public class Dice {
             }
         });
 
-        // 프레임이 키 이벤트를 받을 수 있도록 설정
-        frame.setFocusable(true);
+        setFocusable(true);
 
-        // "Roll Dice" 버튼 클릭 시 주사위 굴리기 시작
-        rollButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                rollTimer.start();
-            }
-        });
+        rollTimer.setInitialDelay(0); // Timer 초기 지연 시간을 0으로 설정
 
-        frame.setSize(400, 400);
-        frame.setVisible(true);
+        setSize(400, 400);
+        setVisible(true);
     }
 
     private void rollDice() {
-        // 주사위 결과에 따라 해당 카드를 보여줌
         int diceResult = (int) (Math.random() * 6) + 1;
         cardLayout.show(cardPanel, String.valueOf(diceResult));
     }
 
-    // 현재 보여지고 있는 카드를 기반으로 현재 주사위 결과를 가져오는 메서드
-    private int getCurrentDiceResult() {
+    private int getCurrentVisibleDiceResult() {
         for (int i = 0; i < diceLabels.length; i++) {
-            // 배열에서 현재 보여지고 있는 JLabel을 찾아 인덱스 반환
             if (diceLabels[i].isVisible()) {
                 return i + 1;
             }
         }
-        return -1; // 에러 처리를 위해 -1 반환
+        return -1;
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+    // 주사위 굴리는 스레드
+    private void startDiceRollThread() {
+        Thread diceRollThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                new Dice();
+                while (rollTimer.isRunning()) {
+                    rollDice(); // 주사위 이미지 변경
+                    try {
+                        Thread.sleep(100); // 이미지 변경 속도
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
+        diceRollThread.start();
+    }
+
+    public int getCurrentDiceResult() {
+        return currentDiceResult;
     }
 }
